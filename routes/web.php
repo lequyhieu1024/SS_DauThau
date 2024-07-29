2b6b4c3b18f2495eace2aad7cfbd7cdd719f7515
Minhle_3107 <lexuanminh3107@gmail.com>
2024-07-29 19:09:03 +0700

    Docs Openapi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/api/docs', [DocsController::class, 'show']);
 
 Route::get('/', function () {
     return view('welcome');
