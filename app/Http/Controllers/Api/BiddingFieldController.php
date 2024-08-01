51f64e404e3c4a3101055f888f02468169beced9
Minhle_3107 <lexuanminh3107@gmail.com>
2024-08-01 22:06:22 +0700

    Edit Openapi Swagger

--- app/Http/Controllers/Api/BiddingFieldController.php
+++ app/Http/Controllers/Api/BiddingFieldController.php
@@ -9,9 +9,176 @@ use App\Http\Requests\BiddingFields\UpdateBiddingFieldRequest;
 use App\Http\Requests\ValidateIdRequest;
 use Illuminate\Support\Facades\DB;
 use App\Models\BiddingField;
+use OpenApi\Annotations as OA;
 
 class BiddingFieldController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/admin/bidding-fields",
+     *     tags={"Bidding Field"},
+     *     summary="Get all bidding fields",
+     *     description="Get all bidding fields",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Limit items per page",
+     *     required=false,
+     *     @OA\Schema(
+     *     type="integer",
+     *     default=10
+     *     )
+     *    ),
+     *     @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="Page number",
+     *     required=false,
+     *     @OA\Schema(
+     *     type="integer",
+     *     default=1
+     *     )
+     *    ),
+     *     @OA\Parameter(
+     *     name="name",
+     *     in="query",
+     *     description="Name of bidding field",
+     *     required=false,
+     *     @OA\Schema(
+     *     type="string"
+     *    )
+     *   ),
+     *     @OA\Parameter(
+     *     name="code",
+     *     in="query",
+     *     description="Code of bidding field",
+     *     required=false,
+     *     @OA\Schema(
+     *     type="string"
+     *   )
+     * ),
+     *     @OA\Parameter(
+     *     name="parent_name",
+     *     in="query",
+     *     description="Parent name of bidding field",
+     *     required=false,
+     *     @OA\Schema(
+     *     type="string"
+     *  )
+     * ),
+     *     @OA\Response(
+     *     response=200,
+     *     description="Get bidding fields successfully",
+     *     @OA\JsonContent(
+     *     type="object",
+     *     @OA\Property(
+     *     property="result",
+     *     type="boolean",
+     *     example=true
+     *     ),
+     *     @OA\Property(
+     *     property="message",
+     *     type="string",
+     *     example="Get bidding fields successfully"
+     *    ),
+     *     @OA\Property(
+     *     property="data",
+     *     type="object",
+     *     @OA\Property(
+     *     property="biddingFields",
+     *     type="array",
+     *     @OA\Items(
+     *     type="object",
+     *     @OA\Property(
+     *     property="id",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="name",
+     *     type="string",
+     *     example="Bidding Field 1"
+     *    ),
+     *     @OA\Property(
+     *     property="description",
+     *     type="string",
+     *     example="Description of bidding field 1"
+     *   ),
+     *     @OA\Property(
+     *     property="code",
+     *     type="string",
+     *     example="BF1"
+     *  ),
+     *     @OA\Property(
+     *     property="is_active",
+     *     type="boolean",
+     *     example=true
+     *     ),
+     *     @OA\Property(
+     *     property="created_at",
+     *     type="string",
+     *     example="2021-09-01T00:00:00.000000Z"
+     *   ),
+     *     @OA\Property(
+     *     property="updated_at",
+     *     type="string",
+     *     example="2021-09-01T00:00:00.000000Z"
+     * ),
+     *     @OA\Property(
+     *     property="parent_id",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="parent_name",
+     *     type="string",
+     *     example="Parent Bidding Field 1"
+     *   )
+     * )
+     * ),
+     *     @OA\Property(
+     *     property="pagination",
+     *     type="object",
+     *     @OA\Property(
+     *     property="currentPage",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="pageSize",
+     *     type="integer",
+     *     example=10
+     *     ),
+     *     @OA\Property(
+     *     property="totalItems",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="totalPages",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="hasNextPage",
+     *     type="boolean",
+     *     example=false
+     *     ),
+     *     @OA\Property(
+     *     property="hasPreviousPage",
+     *     type="boolean",
+     *     example=false
+     *     )
+     * )
+     * )
+     * )
+     * ),
+     * )
+     *
+     *
+     *
+     */
     public function index(IndexBiddingFieldRequest $request)
     {
         $query = BiddingField::getFilteredBiddingFields($request->all());
@@ -69,6 +236,45 @@ class BiddingFieldController extends Controller
         return $branch;
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/admin/bidding-fields/all-ids",
+     *     tags={"Bidding Field"},
+     *     summary="Get all bidding field ids",
+     *     description="Get all bidding field ids",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Response(
+     *     response=200,
+     *     description="Get all successful bidding field ids in tree structure",
+     *     @OA\JsonContent(
+     *     type="object",
+     *     @OA\Property(
+     *     property="result",
+     *     type="boolean",
+     *     example=true
+     *     ),
+     *     @OA\Property(
+     *     property="message",
+     *     type="string",
+     *     example="Get all successful bidding field ids in tree structure"
+     *   ),
+     *     @OA\Property(
+     *     property="data",
+     *     type="array",
+     *     @OA\Items(
+     *     type="object",
+     *     @OA\Property(
+     *     property="id",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     ),
+     *     )
+     *    )
+     *  )
+     * )
+     *
+     */
     public function getAllIds()
     {
         $biddingFields = BiddingField::getAllBiddingFieldIds();
@@ -82,6 +288,113 @@ class BiddingFieldController extends Controller
         ], 200);
     }
 
+
+    /**
+     * @OA\Post(
+     *     path="/api/admin/bidding-fields",
+     *     tags={"Bidding Field"},
+     *     summary="Create a new bidding field",
+     *     description="Create a new bidding field",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(
+     *     required={"name", "description", "code", "is_active"},
+     *     nullable={"parent_id"},
+     *     @OA\Property(
+     *     property="name",
+     *     type="string",
+     *     example="Bidding Field 1"
+     *    ),
+     *     @OA\Property(
+     *     property="description",
+     *     type="string",
+     *     example="Description of bidding field 1"
+     *   ),
+     *     @OA\Property(
+     *     property="code",
+     *     type="number",
+     *     example="666"
+     *  ),
+     *     @OA\Property(
+     *     property="is_active",
+     *     type="number",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="parent_id",
+     *     type="number",
+     *     example=1
+     *     )
+     *   )
+     * ),
+     *     @OA\Response(
+     *     response=201,
+     *     description="Bidding field created successfully",
+     *     @OA\JsonContent(
+     *     type="object",
+     *     @OA\Property(
+     *     property="result",
+     *     type="boolean",
+     *     example=true
+     *     ),
+     *     @OA\Property(
+     *     property="message",
+     *     type="string",
+     *     example="Bidding field created successfully"
+     *   ),
+     *     @OA\Property(
+     *     property="data",
+     *     type="object",
+     *     @OA\Property(
+     *     property="id",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="name",
+     *     type="string",
+     *     example="Bidding Field 1"
+     *   ),
+     *     @OA\Property(
+     *     property="description",
+     *     type="string",
+     *     example="Description of bidding field 1"
+     * ),
+     *     @OA\Property(
+     *     property="code",
+     *     type="number",
+     *     example="666"
+     * ),
+     *     @OA\Property(
+     *     property="is_active",
+     *     type="number",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="created_at",
+     *     type="string",
+     *     example="2021-09-01T00:00:00.000000Z"
+     *  ),
+     *     @OA\Property(
+     *     property="updated_at",
+     *     type="string",
+     *     example="2021-09-01T00:00:00.000000Z"
+     * ),
+     *     @OA\Property(
+     *     property="parent_id",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *    ),
+     *     )
+     *   )
+     * )
+     *
+     *
+     *
+     *
+     */
     public function store(StoreBiddingFieldRequest $request)
     {
         DB::beginTransaction();
@@ -107,6 +420,91 @@ class BiddingFieldController extends Controller
         }
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/admin/bidding-fields/{id}",
+     *     tags={"Bidding Field"},
+     *     summary="Get bidding field by ID",
+     *     description="Get bidding field by ID",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     description="ID of bidding field",
+     *     required=true,
+     *     @OA\Schema(
+     *     type="integer"
+     *   )
+     * ),
+     *     @OA\Response(
+     *     response=200,
+     *     description="Bidding field retrieved successfully",
+     *     @OA\JsonContent(
+     *     type="object",
+     *     @OA\Property(
+     *     property="result",
+     *     type="boolean",
+     *     example=true
+     *     ),
+     *     @OA\Property(
+     *     property="message",
+     *     type="string",
+     *     example="Bidding field retrieved successfully"
+     *  ),
+     *     @OA\Property(
+     *     property="data",
+     *     type="object",
+     *     @OA\Property(
+     *     property="id",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="name",
+     *     type="string",
+     *     example="Bidding Field 1"
+     *  ),
+     *     @OA\Property(
+     *     property="description",
+     *     type="string",
+     *     example="Description of bidding field 1"
+     * ),
+     *     @OA\Property(
+     *     property="code",
+     *     type="number",
+     *     example="666"
+     * ),
+     *     @OA\Property(
+     *     property="is_active",
+     *     type="number",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="created_at",
+     *     type="string",
+     *     example="2021-09-01T00:00:00.000000Z"
+     * ),
+     *     @OA\Property(
+     *     property="updated_at",
+     *     type="string",
+     *     example="2021-09-01T00:00:00.000000Z"
+     * ),
+     *     @OA\Property(
+     *     property="parent_id",
+     *     type="integer",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="parent_name",
+     *     type="string",
+     *     example="Parent Bidding Field 1"
+     *  )
+     * )
+     * )
+     * )
+     * )
+     *
+     */
     public function show(ValidateIdRequest $request)
     {
         $id = $request->route('id');
@@ -123,9 +521,62 @@ class BiddingFieldController extends Controller
             'result' => true,
             'message' => 'Bidding field retrieved successfully',
             'data' => $biddingField,
-        ], 200);
+        ], 201);
     }
 
+    /**
+     * @OA\Patch(
+     *     path="/api/admin/bidding-fields/{id}",
+     *     tags={"Bidding Field"},
+     *     summary="Update bidding field by ID",
+     *     description="Update bidding field by ID",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     description="ID of bidding field",
+     *     required=true,
+     *     @OA\Schema(
+     *     type="integer"
+     *  )
+     * ),
+     *     @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(
+     *     nullable={"parent_id"},
+     *     @OA\Property(
+     *     property="name",
+     *     type="string",
+     *     example="Bidding Field 1"
+     *   ),
+     *     @OA\Property(
+     *     property="description",
+     *     type="string",
+     *     example="Description of bidding field 1"
+     * ),
+     *     @OA\Property(
+     *     property="code",
+     *     type="number",
+     *     example="666"
+     * ),
+     *     @OA\Property(
+     *     property="is_active",
+     *     type="number",
+     *     example=1
+     *     ),
+     *     @OA\Property(
+     *     property="parent_id",
+     *     type="number",
+     *     example=1
+     *     )
+     *  )
+     * ),
+     *     @OA\Response(
+     *     response=200,
+     *     description="Bidding field updated successfully",
+     *     )
+     * )
+     */
     public function update(ValidateIdRequest $request, UpdateBiddingFieldRequest $updateRequest)
     {
         $id = $request->route('id');
@@ -170,6 +621,28 @@ class BiddingFieldController extends Controller
         }
     }
 
+    /**
+     * @OA\Patch(
+     *     path="/api/admin/bidding-fields/{id}/toggle-status",
+     *     tags={"Bidding Field"},
+     *     summary="Toggle active status of bidding field by ID",
+     *     description="Toggle active status of bidding field by ID",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     description="ID of bidding field",
+     *     required=true,
+     *     @OA\Schema(
+     *     type="integer"
+     * )
+     * ),
+     *     @OA\Response(
+     *     response=200,
+     *     description="Bidding field status toggled successfully",
+     *     )
+     * )
+     */
     public function toggleActiveStatus(ValidateIdRequest $request)
     {
         $id = $request->route('id');
@@ -210,6 +683,28 @@ class BiddingFieldController extends Controller
         }
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/admin/bidding-fields/{id}",
+     *     tags={"Bidding Field"},
+     *     summary="Delete bidding field by ID",
+     *     description="Delete bidding field by ID",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     description="ID of bidding field",
+     *     required=true,
+     *     @OA\Schema(
+     *     type="integer"
+     * )
+     * ),
+     *     @OA\Response(
+     *     response=200,
+     *     description="Bidding field deleted successfully",
+     *     )
+     * )
+     */
     public function destroy(ValidateIdRequest $request)
     {
         $id = $request->route('id');
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *    title="APIs Bidding",
+ *    version="1.0.0",
+ * ),
+ * @OA\Tag(
+ *     name="Bidding Field",
+ *     name="Auth",
+ * ),
+ * @OA\SecurityScheme(
+ *       securityScheme="bearerAuth",
+ *       in="header",
+ *       name="bearerAuth",
+ *       type="http",
+ *       scheme="bearer",
+ *       bearerFormat="JWT",
+ *    ),
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
