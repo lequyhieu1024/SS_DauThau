949bf959b348298486a3765faa4476ed36e4d596
Minhle_3107 <lexuanminh3107@gmail.com>
2024-08-01 16:06:20 +0700

    Edit Openapi Swagger

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'API Documentation - Bidding',
             ],
 
             'routes' => [
@@ -145,7 +145,8 @@ return [
              * Allows to generate specs either for OpenAPI 3.0.0 or OpenAPI 3.1.0.
              * By default the spec will be in version 3.0.0
              */
-            'open_api_spec_version' => env('L5_SWAGGER_OPEN_API_SPEC_VERSION', \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION),
+            'open_api_spec_version' => env('L5_SWAGGER_OPEN_API_SPEC_VERSION',
+                \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION),
         ],
 
         /*
