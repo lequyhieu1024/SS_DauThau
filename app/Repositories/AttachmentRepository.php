82cf3e210515e7cbe63abfb7c50a6802ca454eec
trangtth <lequyhieu1024@gmail.com>
2024-12-18 14:36:52 +0700

    fix logic sync attachment

--- app/Repositories/AttachmentRepository.php
+++ app/Repositories/AttachmentRepository.php
@@ -29,6 +29,49 @@ class AttachmentRepository extends BaseRepository
 
     public function createAttachment($files, $projectId, $userId, $projectName)
     {
+        $currentAttachments = $this->model->where('project_id', $projectId)->get();
+
+        if (!empty($currentAttachments))
+        {
+            foreach ($currentAttachments as $currentAttachment) {
+                if ($currentAttachment->path && file_exists($currentAttachment->path)) {
+                    unlink($currentAttachment->path);
+                }
+                $currentAttachment->delete();
+            }
+        }
+
+        $attachments = [];
+
+        foreach ($files as $file) {
+            $newFilename = $projectName . '.' . now()->timestamp . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+            $relativePath = "uploads/documents/{$newFilename}";
+
+            $file->move(public_path('uploads/documents'), $newFilename);
+
+            if (!file_exists(public_path('uploads/documents/' . $newFilename))) {
+                throw new \Exception('Không tìm thấy tập tin sau khi di chuyển.');
+            }
+
+            $data = [
+                'user_id' => $userId,
+                'project_id' => $projectId,
+                'name' => $newFilename,
+                'path' => $relativePath,
+                'type' => $file->getClientOriginalExtension(),
+                'size' => filesize(public_path('uploads/documents/' . $newFilename)),
+                'is_active' => true,
+            ];
+
+            $attachments[] = new AttachmentResource($this->create($data));
+        }
+
+        return $attachments;
+    }
+
+
+    public function createAttachment2($files, $projectId, $userId, $projectName)
+    {
 
         $attachments = [];
 
